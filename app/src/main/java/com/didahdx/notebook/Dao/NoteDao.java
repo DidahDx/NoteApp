1a6a90785b3e7ca8ded158ae499a9dbb7ba2c580
Daniel Didah <didahdaniel@gmail.com>
2019-11-30 20:15:24 +0300

    Added data access object for notes

---
+++ app/src/main/java/com/didahdx/notebook/Dao/NoteDao.java
@@ -0,0 +1,37 @@
+package com.didahdx.notebook.Dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.didahdx.notebook.Models.Note;
+
+import java.util.List;
+
+
+/**
+ * Data Access Objects
+ *
+ * Database Operations to be implemented
+ * */
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+}
