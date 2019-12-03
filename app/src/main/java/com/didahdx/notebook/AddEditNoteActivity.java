aeaf42cd6f4492fb3bde49bdadba44fd21e72893
Daniel Didah <didahdaniel@gmail.com>
2019-12-03 12:27:05 +0300

    Added Edit note functionality

--- app/src/main/java/com/didahdx/notebook/AddEditNoteActivity.java
+++ app/src/main/java/com/didahdx/notebook/AddEditNoteActivity.java
@@ -12,10 +12,12 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
-    public static final String EXTRA_TITLE="EXTRA_TITLE";
-    public static final String EXTRA_DESCRIPTION="EXTRA_DESCRIPTION";
-    public static final String EXTRA_PRIORITY="EXTRA_PRIORITY";
+public class AddEditNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE = "EXTRA_TITLE";
+    public static final String EXTRA_ID = "EXTRA_ID";
+    public static final String EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY = "EXTRA_PRIORITY";
 
     EditText editTextTitle;
     EditText editTextDescrption;
@@ -32,26 +34,38 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPicker.setMaxValue(10);
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Note");
+        if (getIntent() != null) {
+            Intent intent = getIntent();
+            if (intent.hasExtra(EXTRA_ID)) {
+                setTitle("Edit Note");
+                editTextTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+                editTextDescrption.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+                numberPicker.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+            }
+
+        } else {
+
+            setTitle("Add Note");
+        }
     }
 
     private void CollectIds() {
-        editTextTitle=findViewById(R.id.editTextTitle);
-        editTextDescrption=findViewById(R.id.editTextDescription);
-        numberPicker=findViewById(R.id.number_priority);
+        editTextTitle = findViewById(R.id.editTextTitle);
+        editTextDescrption = findViewById(R.id.editTextDescription);
+        numberPicker = findViewById(R.id.number_priority);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.add_note,menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.save_note:
                 SaveNote();
                 return true;
@@ -61,21 +75,27 @@ public class AddNoteActivity extends AppCompatActivity {
     }
 
     private void SaveNote() {
-        String title=editTextTitle.getText().toString().trim();
-        String description=editTextDescrption.getText().toString().trim();
-        int priority= numberPicker.getValue();
+        String title = editTextTitle.getText().toString().trim();
+        String description = editTextDescrption.getText().toString().trim();
+        int priority = numberPicker.getValue();
 
-        if (title.isEmpty() || description.isEmpty()){
-            Toast.makeText(this,"Insert a title and description",Toast.LENGTH_SHORT).show();
+        if (title.isEmpty() || description.isEmpty()) {
+            Toast.makeText(this, "Insert a title and description", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        Intent data =new Intent();
-        data.putExtra(EXTRA_TITLE,title);
-        data.putExtra(EXTRA_DESCRIPTION,description);
-        data.putExtra(EXTRA_PRIORITY,priority);
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
 
-        setResult(RESULT_OK,data);
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+
+        if (id != -1) {
+            data.putExtra(EXTRA_ID, id);
+        }
+
+        setResult(RESULT_OK, data);
         finish();
     }
 }
