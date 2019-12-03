ce73b24be186696fe4b8b5181b3b87194a1677ba
Daniel Didah <didahdaniel@gmail.com>
2019-12-03 08:09:01 +0300

    added swipe to delete

--- app/src/main/java/com/didahdx/notebook/MainActivity.java
+++ app/src/main/java/com/didahdx/notebook/MainActivity.java
@@ -1,16 +1,99 @@
 package com.didahdx.notebook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.didahdx.notebook.Adapters.NoteAdapter;
+import com.didahdx.notebook.Models.Note;
+import com.didahdx.notebook.ViewModel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final int ADD_NOTE_REQUEST=1;
+    private NoteViewModel noteViewModel;
+    RecyclerView recyclerView;
+    NoteAdapter noteAdapter;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FindIds();
 
+        floatingActionButton.setOnClickListener(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        noteAdapter=new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
+        noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                noteAdapter.setNotes(notes);
+            }
+        });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT){
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                noteViewModel.delete(noteAdapter.getNoteAt(viewHolder.getAdapterPosition()));
+                Toast.makeText(MainActivity.this, "Note at position "+viewHolder.getAdapterPosition()+" deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
+    }
+
+    private void FindIds() {
+        recyclerView=findViewById(R.id.recycler_view);
+        floatingActionButton=(FloatingActionButton) findViewById(R.id.button_add_note);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v==floatingActionButton) {
+            startActivityForResult(new Intent(MainActivity.this,AddNoteActivity.class),ADD_NOTE_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==ADD_NOTE_REQUEST && resultCode==RESULT_OK){
+            assert data != null;
+            String title=data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description=data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority=data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            noteViewModel.insert(new Note(title,description,priority));
+
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
